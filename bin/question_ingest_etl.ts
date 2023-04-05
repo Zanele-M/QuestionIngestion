8f54ef5f42b95eb8dca8ab140260b1b1ee0cb080
Zanele-M <zsp2220696.sibanyoni@gmail.com>
2023-04-05 10:06:29 +0200

    adding pipeline statck

--- bin/question_ingest_etl.ts
+++ bin/question_ingest_etl.ts
@@ -1,21 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { QuestionIngestEtlStack } from '../lib/question_ingest_etl-stack';
+import { QuestionIngestEtlStack } from '../lib/question-ingestion-etl-stack';
+import { QuestionIngestionPipelineStack } from '../lib/question-ingestion-pipeline-stack;
+
 
 const app = new cdk.App();
-new QuestionIngestEtlStack(app, 'QuestionIngestEtlStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+new QuestionIngestEtlStack(app, 'QuestionIngestEtlStack', {});
+new QuestionIngestionPipelineStack(app, "PipelineStack", {});
