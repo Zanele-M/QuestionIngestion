552f5fd72c8b97ce2872f16be1147d667befb552
Zanele-M <zsp2220696.sibanyoni@gmail.com>
2023-04-07 16:10:14 +0200

    fix

--- lib/pipelinestack.ts
+++ lib/pipelinestack.ts
@@ -4,7 +4,6 @@ import { Construct } from 'constructs';
 import { CloudFormationCreateUpdateStackAction, CodeBuildAction, GitHubSourceAction } from "aws-cdk-lib/aws-codepipeline-actions";
 import { BuildSpec, LinuxBuildImage, PipelineProject } from "aws-cdk-lib/aws-codebuild";
 
-
 export class PipelineStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
       super(scope, id, props);
@@ -49,7 +48,7 @@ pipeline.addStage({
     stageName: "Build",
     actions: [
         new CodeBuildAction({
-            actionName: "Build",
+            actionName: "Pipeline_Build",
             project: new PipelineProject(this, "Build", {
                 buildSpec: BuildSpec.fromSourceFilename("build-specs/cdk-build-spec.yml"),
                 environment: {
@@ -61,7 +60,7 @@ pipeline.addStage({
         }),
         //add another build action for the cdk service code
         new CodeBuildAction({   
-            actionName: "ServerBuild",
+            actionName: "ApplicationServer_Build",
             project: new PipelineProject(this, "ServerBuildProject", {
                 buildSpec: BuildSpec.fromSourceFilename("build-specs/server-build-spec.yml"),
                 environment: {
@@ -87,6 +86,5 @@ pipeline.addStage({
         }),
     ],
 });
-
     }
   }
